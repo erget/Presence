2e5d242461555b2f10df5beae5a3d649f8dfc4ba
Daniel Lee <erget2005@gmail.com>
2017-06-10 19:40:01 +0200

    CODA_Aware lives

--- presence/coda_aware.py
+++ presence/coda_aware.py
@@ -1,14 +1,25 @@
 """Tools for dealing with EUMETSAT's CODA server."""
 
+import requests
+from requests.auth import HTTPBasicAuth
+
+
 class CODA_Aware(object):
 
     """
     A class that sends authenticated queries to CODA.
     """
 
+    #: Authentification details. This should be configured, not in sources!
+    user = "hackathon12"
+    password = "copernicus"
+    SERVICE_ROOT = "https://coda.eumetsat.int/odata/v1/"
+
     def __init__(self):
-        pass
+        pass #raise NotImplementedError()
 
     def query(self, query_string):
         """Stream authenticated response from CODA OData v1 API."""
-        return StreamingResponse
+        return requests.get(self.SERVICE_ROOT + query_string,
+                            auth=HTTPBasicAuth(self.user, self.password),
+                            verify=False, stream=True)
