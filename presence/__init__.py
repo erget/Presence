cf5c4a736f26b04f12ccf126c83b169bb2e052db
Daniel Lee <erget2005@gmail.com>
2017-06-11 11:10:21 +0200

    Implement Product getter

---
+++ presence/__init__.py
@@ -0,0 +1,3 @@
+from .coda_aware import CODA_Aware
+from .product import Product
+from .query import Query
--- presence/product.py
+++ presence/product.py
@@ -9,39 +9,39 @@ class Product(CODA_Aware):
 
     """A CODA product, composed mainly of NetCDF files."""
 
-    def __init__(self, uuid):
+    def __init__(self, uuid, work_dir=""):
+        self.work_dir = work_dir
         self.uuid = uuid
         uuid_query = "Products('{}')".format(self.uuid)
-        res = self.query(uuid_query)
-        dom = parseString(res.text)
 
         nodes = self.query(uuid_query + "/Nodes")
         dom = parseString(nodes.text)
         self.root = dom.getElementsByTagName("title").item(
             1).firstChild.nodeValue
 
-        manifest_query = (uuid_query +
-                          "/Nodes('{}')/Nodes("
-                          "'xfdumanifest.xml')/$value".format(self.root))
-        manifest = parseString(self.query(manifest_query).text)
-        self.files = [x.attributes.get("href").nodeValue for x in
-                      manifest.getElementsByTagName("fileLocation")]
-
         try:
-            os.makedirs(self.root)
+            os.makedirs(os.path.join(work_dir, self.root))
         except FileExistsError:
             pass
 
+        manifest_file = self.get("xfdumanifest.xml")
+        print(manifest_file)
+        with open(manifest_file) as manifest:
+            manifest_dom = parseString(manifest.read())
+        self.files = [x.attributes.get("href").nodeValue for x in
+                      manifest_dom.getElementsByTagName("fileLocation")]
 
     def get(self, filename):
         """If needed, retrieve, and return absolute path to file."""
-        path = os.path.join(self.root, filename)
+        path = os.path.join(self.work_dir, self.root, filename)
         if not os.path.exists(path):
-            pass  # Get the file from the server and put it there
-        else:
-            return path
+            # Get the file from the server and put it there
+            result = self.query("Products('{}')/Nodes('{}')/Nodes('{"
+                                "}')/$value".format(self.uuid, self.root,
+                                                    filename))
+            with open(path, "wb") as f:
+                for chunk in result.iter_content(chunk_size=1024):
+                    if chunk:
+                        f.write(chunk)
+        return path
 
-        """
-        You can turn the manifest query into something more general and just
-        use it in order to get the node xfdumanifest.xml.
-        """
